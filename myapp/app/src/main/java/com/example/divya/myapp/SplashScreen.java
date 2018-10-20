6d80747082fb4a8167e596023fdea41320e355f8
yulanl22 <yulanl_22@rediffmail.com>
2018-10-20 19:07:37 +0530

    First commit

---
+++ myapp/app/src/main/java/com/example/divya/myapp/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.divya.myapp;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private int SLEEP_TIMER = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+
+    }
+
+    private class LogoLauncher extends Thread{
+        public void run() {
+            try {
+                sleep(1000 * SLEEP_TIMER);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            SplashScreen.this.finish();
+
+        }
+    }
+
+}
